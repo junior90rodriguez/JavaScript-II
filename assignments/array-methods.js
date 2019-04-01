d53cf98cf712b1b84378ab7acf6728dc4a2ed348
Alberto Rodriguez <juniorrodriguez@Albertos-iMac.local>
2019-03-31 21:11:21 -0500

    Closure challange 1 is complete

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -83,10 +83,6 @@ const ticketPriceTotal = runners.reduce( function(accumulator, currentValue){
 }, 0);
 console.log(ticketPriceTotal);
 
-// const totalLandArea = cityData.reduce( function(accumulator, currentValue){
-//   return accumulator + currentValue.land_area;
-// }, 0);//0 is used as a starting point
-
 // console.log(totalLandArea);
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
